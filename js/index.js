f036aabbb0f058be6e8aa2439b0a043269689666
Rifki <rifki.ramdani330@gmail.com>
2025-06-29 03:41:26 +0700

    Refactor header navigation for improved accessibility and responsiveness; add off-screen menu functionality with JavaScript. (refine navbar)

--- js/index.js
+++ js/index.js
@@ -1,13 +1,3 @@
-const a965_header_hammenu = document.querySelector('.a965_header_hammenu');
-
-const off_screen_menu = document.querySelector('.off_screen_menu');
-
-a965_header_hammenu.addEventListener('click', () => {
-    a965_header_hammenu.classList.toggle('active');
-    off_screen_menu.classList.toggle('active');
-    document.body.classList.toggle('no-scroll');
-})
-
 /*festivals-frame animation*/
 const left = document.querySelector('.a965_frame1_content_left');
 const frame = document.querySelector('.a965_frame1');
